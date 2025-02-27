8fe12a7bf2894884490e3e69ea2bba9c3b8eb9b3
venkataguruprasadG <venkatagp27@gmail.com>
2025-02-27 17:16:44 +0530

    day-2 even  or odd

---
+++ day-2-even-or-odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+    if(num%2==0){
+        printf("Even");
+    }
+    else{
+        printf("Odd");
+    }
+    return 0;
+}
